237a24d3225315a8edce9222c62385fa19eb7aa6
Korede <olakorede.salami@gmail.com>
2020-04-17 09:20:51 +0100

    Updates

---
+++ precache-manifest.2bab908bf374d3e7f26b55532065248d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb6f4447b113cc33c0a3878c5bcd772",
+    "url": "/React-Meme-Generator/index.html"
+  },
+  {
+    "revision": "8726b1e55d545ee87723",
+    "url": "/React-Meme-Generator/static/css/main.73882827.chunk.css"
+  },
+  {
+    "revision": "79e98b7a5ca4d7147135",
+    "url": "/React-Meme-Generator/static/js/2.df068a34.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-Meme-Generator/static/js/2.df068a34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8726b1e55d545ee87723",
+    "url": "/React-Meme-Generator/static/js/main.04f5ba2b.chunk.js"
+  },
+  {
+    "revision": "1f1fca6f2aec11cf59f3",
+    "url": "/React-Meme-Generator/static/js/runtime-main.f433c9cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Meme-Generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Meme-Generator/precache-manifest.b53684bdc1cd94854e5d177dd1f5e110.js"
+  "/React-Meme-Generator/precache-manifest.2bab908bf374d3e7f26b55532065248d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Meme-Generator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Meme-Generator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
